0985c2e128e75a4e85b5ca28e59c26bc0106568b
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2019-03-27 05:07:04 +0900

    initial import

---
+++ docker/app/docker-entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [[ $1 = "start-server" ]]; then
+  rm /app/tmp/pids/*
+
+  bundle install -j8 --path vendor/bundle
+
+  yarn install
+
+  if [[ $NODE_ENV = "production" ]]; then
+    bundle exec rails webpacker:compile
+  fi
+
+  rm -rf /var/www/*
+  cp -r /app/public/* /var/www/
+
+  if [[ $RAILS_ENV != "production" && -f Procfile.dev ]]; then
+    bundle exec foreman start -f Procfile.dev
+  else
+    bundle exec foreman start
+  fi
+else
+  exec "$@"
+fi
