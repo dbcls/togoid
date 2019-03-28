da7bb759653eacc36c60423b743f0468bfc347f9
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2019-03-28 14:29:51 +0900

    add route for convert

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # API
+  get 'convert', to: 'api#convert', defaults: { format: 'json' }
 end
