0ce78da79e161f2ff3c001dffe3af417b3833649
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2019-04-09 12:08:34 +0900

    add list of selectors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  module SearchMethods
+    Item = Struct.new(:label, :path)
+
+    Rails.application.routes.url_helpers.yield_self do |helper|
+      IDNAVIGATOR = Item.new('ID Navigator', helper.root_path)
+      IDCONVERTER = Item.new('ID Converter', '#')
+      IDRESOLVER = Item.new('ID Resolver', '#')
+    end
+
+    def self.each
+      [IDNAVIGATOR, IDCONVERTER, IDRESOLVER].each { |x| yield x } if block_given?
+    end
+  end
 end
