42df846c5766a893c11d5ffc4aa5fd37c9439bf5
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2019-04-09 18:48:56 +0900

    enable header link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,13 +3,13 @@ module ApplicationHelper
     Item = Struct.new(:label, :path)
 
     Rails.application.routes.url_helpers.yield_self do |helper|
-      IDNAVIGATOR = Item.new('ID Navigator', helper.root_path)
-      IDCONVERTER = Item.new('ID Converter', '#')
-      IDRESOLVER = Item.new('ID Resolver', '#')
+      ID_NAVIGATOR = Item.new('ID Navigator', helper.root_path)
+      ID_CONVERTER = Item.new('ID Converter', helper.converter_path)
+      ID_RESOLVER = Item.new('ID Resolver', helper.resolver_path)
     end
 
     def self.each
-      [IDNAVIGATOR, IDCONVERTER, IDRESOLVER].each { |x| yield x } if block_given?
+      [ID_NAVIGATOR, ID_CONVERTER, ID_RESOLVER].each { |x| yield x } if block_given?
     end
   end
 end
