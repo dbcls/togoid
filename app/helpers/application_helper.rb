15f97376e1f6171e21f1a583dfc8d987129b9766
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2019-04-16 16:42:17 +0900

    fix load error

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,9 @@ module ApplicationHelper
   module SearchMethods
     Item = Struct.new(:label, :path)
 
-    Rails.application.routes.url_helpers.yield_self do |helper|
-      ID_NAVIGATOR = Item.new('ID Navigator', helper.root_path)
-      ID_CONVERTER = Item.new('ID Converter', helper.converter_path)
-      ID_RESOLVER = Item.new('ID Resolver', helper.resolver_path)
-    end
+    IDNAVIGATOR = Item.new('ID Navigator', :root_path)
+    IDCONVERTER = Item.new('ID Converter', :convert_index_path)
+    IDRESOLVER = Item.new('ID Resolver', :convert_index_path)
 
     def self.each
       [ID_NAVIGATOR, ID_CONVERTER, ID_RESOLVER].each { |x| yield x } if block_given?
