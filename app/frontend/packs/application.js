a7ca01763d924eeb396606dd4718439770c300d8
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2019-04-16 19:28:58 +0900

    enable js-routes

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -3,3 +3,6 @@ require.context('../images', true, /\.(png|jpg|jpeg|svg)$/);
 import 'stylesheets/application';
 
 import 'bootstrap/dist/js/bootstrap';
+
+import Routes from '../javascripts/js-routes.js.erb';
+window.Routes = Routes;
