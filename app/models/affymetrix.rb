d5b020e8d499079adc4d090d37083f24250413ea
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2019-04-18 02:43:08 +0900

    initial import

---
+++ app/models/affymetrix.rb
@@ -0,0 +1,29 @@
+class Affymetrix
+  include Queryable
+
+  class << self
+    def find(id)
+      query = client.select
+                 .from(RDF::URI('http://togoid.dbcls.jp/graph/affymetrix'))
+                 .where([RDF::URI("http://identifiers.org/affy.probeset/#{id}"), :p, :o])
+
+      raise ResourceNotFound.new(nil, self, id) if query.result.count.zero?
+
+      new do |attr|
+        attr.id = id
+      end
+    end
+  end
+
+  attr_accessor :id
+
+  def initialize(**attributes)
+    attributes.each { |k, v| send("#{k}=", v) }
+
+    yield self if block_given?
+  end
+
+  def convert
+    HGNC.convert(self)
+  end
+end
